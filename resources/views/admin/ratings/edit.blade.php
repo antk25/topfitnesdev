b9e54627e57e3ffe3ead773c1d833b1f526f5cf0
Anton Kunaev <kunaevanton25@gmail.com>
2021-10-21 21:30:11 +0300

    Добавлен редактор кода для редактирования рейтинга

--- resources/views/admin/ratings/edit.blade.php
+++ resources/views/admin/ratings/edit.blade.php
@@ -3,16 +3,90 @@
 @section('content')
 
 <div class="container max-width-md">
-  
-    
-  @livewire('admin.bracelets-in-rating', ['rating' => $rating, 'edit' => 1])
-    
+
+
+    <form class="form-template-v3" method="POST" action="{{ route('ratings.update', ['rating' => $rating->id]) }}">
+    @csrf
+    @method('PUT')
+        <div class="grid gap-xxs margin-bottom-xs">
+        <div class="col-6@md">
+          <label class="form-label margin-bottom-xxs" for="name">Название рейтинга</label>
+          <input class="form-control width-100%" type="text" name="name" id="name" value="{{ $rating->name }}">
+          <p class="text-xs color-contrast-medium margin-top-xxs">Короткое название, menutitle</p>
+        </div>
+
+        <div class="col-6@md">
+          <label class="form-label margin-bottom-xxs" for="slug">URI (SLUG)</label>
+        <input class="form-control width-100%" type="text" name="slug" id="slug" value="{{ $rating->slug }}">
+        </div>
+      </div>
+
+      <div class="margin-bottom-xs">
+        <label class="form-label margin-bottom-xxs" for="title">Title</label>
+        <input class="form-control width-100%" type="text" name="title" id="title" value="{{ $rating->title }}">
+      </div>
+
+      <div class="grid gap-xxs margin-bottom-xs">
+        <div class="col-6@md">
+          <label class="form-label margin-bottom-xxs" for="subtitle">Subtitle (h1)</label>
+          <input class="form-control width-100%" type="text" name="subtitle" id="subtitle" value="{{ $rating->subtitle }}">
+        </div>
+        <div class="col-6@md">
+          <div class="character-count js-character-count">
+            <label class="form-label margin-bottom-xxs" for="textareaName">Description:</label>
+            <textarea class="form-control width-100% js-character-count__input" name="description" id="description" maxlength="300">{{ $rating->description }}</textarea>
+            <div class="character-count__helper character-count__helper--dynamic text-sm margin-top-xxxs" aria-live="polite" aria-atomic="true">
+              Осталось <span class="js-character-count__counter"></span> символов
+            </div>
+            <div class="character-count__helper character-count__helper--static text-sm margin-top-xxxs">Макс 300 символов</div>
+          </div>
+        </div>
+      </div>
+
+      <div class="margin-bottom-sm">
+        <label class="form-label margin-bottom-xxs" for="text">Основной контент (в начале статьи)</label>
+            <textarea rows="20" class="form-control width-100% text-sm" spellcheck="false" name="text" id="text">{{ $rating->text }}</textarea>
+      </div>
+
+      <button class="btn btn--primary" type="submit">Сохранить</button>
+
+    </form>
+
 </div>
 
 @endsection
 
 @section('scripts')
 @parent
-{{-- <script src="{{ asset("js/admin/alpine.min.js") }}"></script> --}}
+<script src="{{ asset("js/admin/alpine.min.js") }}"></script>
+
+<script src="{{ asset("js/admin/codemirror.min.js") }}"></script>
+    <script src="{{ asset("js/admin/xml-fold.js") }}"></script>
+    <script src="{{ asset("js/admin/closetag.js") }}"></script>
+    <script src="{{ asset("js/admin/matchtags.js") }}"></script>
+    <script src="{{ asset("js/admin/trailingspace.js") }}"></script>
+    <script src="{{ asset("js/admin/xml.js") }}"></script>
+    <script src="{{ asset("js/admin/fullscreen.js") }}"></script>
+    <script>
+      var myCodeMirror = CodeMirror.fromTextArea((text), {
+        lineNumbers: true,
+        tabSize: 2,
+        mode: "text/html",
+        autoCloseTags: true,
+        lineWrapping: true,
+        matchTags: {bothTags: true},
+        extraKeys: {"Ctrl-J": "toMatchingTag"},
+        showTrailingSpace: true,
+        extraKeys: {
+        "F11": function(cm) {
+          cm.setOption("fullScreen", !cm.getOption("fullScreen"));
+        },
+        "Esc": function(cm) {
+          if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
+        }
+      }
+      });
+
+    </script>
 
 @endsection
