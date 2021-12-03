1927cabb655938cbea8c51d2097c4738eb0f8523
Anton Kunaev <kunaevanton25@gmail.com>
2021-12-03 23:26:26 +0300

    Настройка добавления/удаления оценок

--- resources/views/admin/grades/create.blade.php
+++ resources/views/admin/grades/create.blade.php
@@ -2,59 +2,85 @@
 
 @section('content')
 
+<div class="margin-bottom-md">
+  <h1 class="text-lg">Новая оценка</h1>
+</div>
+
+{{-- Сообщение об успешности сохранения --}}
+@if(session('success'))
+
+<div class="alert alert--success alert--is-visible padding-sm radius-md js-alert margin-y-sm" role="alert">
+  <div class="flex items-center justify-between">
+    <div class="flex items-center">
+      <svg class="icon icon--sm alert__icon margin-right-xxs" viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M12,0A12,12,0,1,0,24,12,12.035,12.035,0,0,0,12,0ZM10,17.414,4.586,12,6,10.586l4,4,8-8L19.414,8Z"></path>
+      </svg>
+
+      <p class="text-sm"><strong>Успешно:</strong> {{ session('success') }}.</p>
+    </div>
+
+    <button class="reset alert__close-btn margin-left-sm js-alert__close-btn js-tab-focus">
+      <svg class="icon" viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+        <title>Close alert</title>
+        <line x1="3" y1="3" x2="17" y2="17" />
+        <line x1="17" y1="3" x2="3" y2="17" />
+      </svg>
+    </button>
+  </div>
+</div>
+@endif
+{{-- Конец сообщения об успешности сохранения --}}
+
+
+{{-- Сообщения об ошибках --}}
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+{{-- Конец сообщения об ошибках --}}
+
 <form class="form-template-v3" method="POST" action="{{ route('grades.store') }}">
     @csrf
+    <div class="bg radius-md shadow-xs padding-md margin-bottom-md">
     <fieldset class="margin-bottom-md padding-bottom-md border-bottom">
-      <div class="text-component margin-bottom-md text-center">
-        <h2>Создать оценку</h2>
-            <p>Управление оценками для браслетов.</p>
-      </div>
-  
+
       <div class="margin-bottom-xs">
         <div class="grid gap-xxs items-center@md">
           <div class="col-4@md">
             <label class="form-label" for="name">Название оценки</label>
           </div>
-          
+
           <div class="col-8@md">
-            <input class="form-control width-100%" type="text" name="name" id="name" required>
+            <input class="form-control width-100%" type="text" name="name" id="name" value="{{ old('name') }}">
           </div>
         </div>
       </div>
-  
+
       <div>
         <div class="grid gap-xxs items-center@md">
           <div class="col-4@md">
             <label class="form-label" for="about">Описание</label>
             <p class="text-xs color-contrast-medium margin-top-xxxxs">Опционально</p>
           </div>
-          
+
           <div class="col-8@md">
-            <textarea class="form-control width-100%" name="about" id="code"></textarea>
+            <textarea class="form-control width-100%" name="about" id="code">{{ old('about') }}</textarea>
           </div>
         </div>
       </div>
     </fieldset>
-  
+    </div>
+
     <div class="text-right">
-      <button type="submit" class="btn btn--primary">Отправить</button>
+      <button type="submit" class="btn btn--primary">Сохранить</button>
     </div>
   </form>
 @endsection
 
 @section('scripts')
-@parent
-<script src="{{ asset("js/admin/codemirror.js") }}"></script>
-    <script src="{{ asset("js/admin/closetag.js") }}"></script>
-    <script src="{{ asset("js/admin/htmlmixed.js") }}"></script>
-    <script src="{{ asset("js/admin/css.js") }}"></script>
-    <script src="{{ asset("js/admin/javascript.js") }}"></script>
-    <script src="{{ asset("js/admin/xml.js") }}"></script>
-    <script src="{{ asset("js/admin/xml-fold.js") }}"></script>
-    <script>
-      var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-        mode: 'text/html',
-        autoCloseTags: true
-      });
-    </script>    
 @endsection
--- resources/views/admin/grades/edit.blade.php
+++ resources/views/admin/grades/edit.blade.php
@@ -2,61 +2,87 @@
 
 @section('content')
 
+<div class="margin-bottom-md">
+  <h1 class="text-lg">Редактирование оценки id({{ $grade->id }})</h1>
+</div>
+
+{{-- Сообщение об успешности сохранения --}}
+@if(session('success'))
+
+<div class="alert alert--success alert--is-visible padding-sm radius-md js-alert margin-y-sm" role="alert">
+  <div class="flex items-center justify-between">
+    <div class="flex items-center">
+      <svg class="icon icon--sm alert__icon margin-right-xxs" viewBox="0 0 24 24" aria-hidden="true">
+        <path d="M12,0A12,12,0,1,0,24,12,12.035,12.035,0,0,0,12,0ZM10,17.414,4.586,12,6,10.586l4,4,8-8L19.414,8Z"></path>
+      </svg>
+
+      <p class="text-sm"><strong>Успешно:</strong> {{ session('success') }}.</p>
+    </div>
+
+    <button class="reset alert__close-btn margin-left-sm js-alert__close-btn js-tab-focus">
+      <svg class="icon" viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2">
+        <title>Close alert</title>
+        <line x1="3" y1="3" x2="17" y2="17" />
+        <line x1="17" y1="3" x2="3" y2="17" />
+      </svg>
+    </button>
+  </div>
+</div>
+@endif
+{{-- Конец сообщения об успешности сохранения --}}
+
+
+{{-- Сообщения об ошибках --}}
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+{{-- Конец сообщения об ошибках --}}
+
 <form class="form-template-v3" method="POST" action="{{ route('grades.update', ['grade' => $grade->id]) }}">
     @csrf
     @method('PUT')
+    <div class="bg radius-md shadow-xs padding-md margin-bottom-md">
     <fieldset class="margin-bottom-md padding-bottom-md border-bottom">
-      <div class="text-component margin-bottom-md text-center">
-        <h2>Изменить оценку {{ $grade->name }}</h2>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      </div>
-  
+
       <div class="margin-bottom-xs">
         <div class="grid gap-xxs items-center@md">
-          <div class="col-2@md">
+          <div class="col-4@md">
             <label class="form-label" for="name">Название оценки</label>
           </div>
-          
-          <div class="col-10@md">
+
+          <div class="col-8@md">
             <input class="form-control width-100%" type="text" name="name" id="name" value="{{ $grade->name }}">
           </div>
         </div>
       </div>
- 
+
       <div>
         <div class="grid gap-xxs items-center@md">
-          <div class="col-2@md">
+          <div class="col-4@md">
             <label class="form-label" for="about">Описание</label>
             <p class="text-xs color-contrast-medium margin-top-xxxxs">Опционально</p>
           </div>
-          
-          <div class="col-10@md">
+
+          <div class="col-8@md">
             <textarea class="form-control width-100%" name="about" id="code">{{ $grade->about }}</textarea>
           </div>
         </div>
       </div>
     </fieldset>
-  
+    </div>
+
     <div class="text-right">
-      <button type="submit" class="btn btn--primary">Отправить</button>
+      <button type="submit" class="btn btn--primary">Сохранить</button>
     </div>
   </form>
 @endsection
 
-l
 @section('scripts')
-@parent
-<script src="{{ asset("js/admin/codemirror.js") }}"></script>
-    <script src="{{ asset("js/admin/closetag.js") }}"></script>
-    <script src="{{ asset("js/admin/htmlmixed.js") }}"></script>
-    <script src="{{ asset("js/admin/css.js") }}"></script>
-    <script src="{{ asset("js/admin/javascript.js") }}"></script>
-    <script src="{{ asset("js/admin/xml.js") }}"></script>
-    <script src="{{ asset("js/admin/xml-fold.js") }}"></script>
-    <script>
-      var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-        mode: 'text/html',
-        autoCloseTags: true
-      });
-    </script>    
+
 @endsection
