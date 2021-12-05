24fd9dcadd849b814b0e04ebf9667f174279b1e8
Anton Kunaev <kunaevanton25@gmail.com>
2021-12-05 23:08:36 +0300

    Настройка формы для продавцов

--- resources/views/admin/sellers/create.blade.php
+++ resources/views/admin/sellers/create.blade.php
@@ -2,19 +2,25 @@
 
 @section('content')
 
+<div class="margin-bottom-md">
+  <h1 class="text-lg">Новый продавец</h1>
+</div>
+
 <form class="form-template-v3" method="POST" action="{{ route('sellers.store') }}">
     @csrf
-    <fieldset class="margin-bottom-md padding-bottom-md border-bottom">
-      <div class="text-component margin-bottom-md text-center">
-        <h2>Добавить продавца</h2>
-            <p>Управление продавцами и партнерскими программами для браслетов.</p>
-      </div>
+    <div class="bg radius-md shadow-xs padding-md margin-bottom-md">
+    <fieldset>
 
-      <div class="grid gap-xxs margin-bottom-xs">
-        <div class="col-4@md">
-          <label class="form-label margin-bottom-xxxs" for="marketplace">Выбрать, если маркетплейс</label>
+      <div class="margin-bottom-xs">
+        <div class="grid gap-xxs items-center@md">
+          <div class="col-4@md">
 
-          <div class="select">
+          <label class="form-label" for="marketplace">Выбрать, если маркетплейс</label>
+
+          </div>
+
+          <div class="col-8@md">
+            <div class="select">
             <select class="select__input btn btn--subtle" name="marketplace" id="marketplace">
                 <option value="">Выбрать маркетплейс</option>
                 <option value="aliexpress">Aliexpress</option>
@@ -22,26 +28,50 @@
             </select>
 
             <svg class="icon select__icon" aria-hidden="true" viewBox="0 0 16 16"><polyline points="1 5 8 12 15 5" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"/></svg>
+            </div>
           </div>
         </div>
-        <div class="col-4@md">
-          <label class="form-label margin-bottom-xxs" for="name">Название продавца</label>
-          <input class="form-control width-100%" type="text" name="name" id="name">
-          <p class="text-xs color-contrast-medium margin-top-xxs">Название магазина (если маркетплейс, то можно просто продавец 1, продавец 2. Выводится только, если это не маркетплейс)</p>
-        </div>
+      </div>
 
+      <div class="margin-bottom-xs">
+        <div class="grid gap-xxs items-center@md">
           <div class="col-4@md">
-            <label class="form-label margin-bottom-xxs" for="about">Описание:</label>
+
+         <label class="form-label" for="name">Название продавца</label>
+
+          <p class="text-xs color-contrast-medium margin-top-xxs">Название магазина (если маркетплейс, то можно просто продавец 1, продавец 2. Выводится только, если это не маркетплейс)</p>
+          </div>
+
+          <div class="col-8@md">
+
+          <input class="form-control width-100%" type="text" name="name" id="name">
+
+          </div>
+        </div>
+      </div>
+
+      <div class="margin-bottom-xs">
+        <div class="grid gap-xxs items-center@md">
+          <div class="col-4@md">
+
+            <label class="form-label" for="about">Описание:</label>
             <p class="text-xs color-contrast-medium margin-top-xxxxs">Опционально</p>
 
-            <textarea class="form-control width-100%" name="about" id="about"></textarea>
+          </div>
+
+          <div class="col-8@md">
+
+          <textarea class="form-control width-100%" name="about" id="about"></textarea>
 
           </div>
+        </div>
       </div>
+
     </fieldset>
+    </div>
 
     <div class="text-right">
-      <button type="submit" class="btn btn--primary">Отправить</button>
+      <button type="submit" class="btn btn--primary">Сохранить</button>
     </div>
   </form>
 @endsection
