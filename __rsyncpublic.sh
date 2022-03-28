9c2ca398b55864a103445eccb5dde940d293ca76
Anton Kunaev <kunaevanton25@gmail.com>
2022-03-28 21:12:46 +0300

    Добавить новый список для дизайна перелинковки

--- __rsyncpublic.sh
+++ __rsyncpublic.sh
@@ -1,5 +1,6 @@
  #!/bin/bash
 
-rsync -avzhe ssh --progress public/css topfitnesbraslet@62.113.100.14:~/public_html/current/public/css
-rsync -avzhe ssh --progress public/js topfitnesbraslet@62.113.100.14:~/public_html/current/public/js
-rsync -avzhe ssh --progress public/img topfitnesbraslet@62.113.100.14:~/public_html/current/public/img
+# rsync -avzhe ssh --progress public/css topfitnesbraslet@62.113.100.14:~/public_html/current/public/css
+# rsync -avzhe ssh --progress public/js topfitnesbraslet@62.113.100.14:~/public_html/current/public/js
+rsync -avzhe ssh --progress topfitnesbraslet@62.113.100.14:~/public_html/current/public/img public/img
+rsync -avzhe ssh --progress topfitnesbraslet@62.113.100.14:~/public_html/current/public/storage public/storage
