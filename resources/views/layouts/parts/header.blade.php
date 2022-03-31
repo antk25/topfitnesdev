2142f0fd9fedbdbd68e68ba178c84d335846a7a8
Anton Kunaev <kunaevanton25@gmail.com>
2022-03-31 16:52:50 +0300

    Создать логотип

--- resources/views/layouts/parts/header.blade.php
+++ resources/views/layouts/parts/header.blade.php
@@ -1,16 +1,13 @@
 <header class="header-v2 js-header-v2" data-animation="on" data-animation-offset="600">
     <div class="header-v2__wrapper">
         <div class="header-v2__container container max-width-lg">
-            <div class="header-v2__logo">
+            <div class="header-v2__logo flex flex-center">
                 <a href="/">
-                    <svg width="104" height="30" viewBox="0 0 104 30">
-                        <title>Go to homepage</title>
-                        <path
-                            d="M37.54 24.08V3.72h4.92v16.37h8.47v4zM60.47 24.37a7.82 7.82 0 01-5.73-2.25 8.36 8.36 0 01-2-5.62 8.32 8.32 0 012.08-5.71 8 8 0 015.64-2.18 8.07 8.07 0 015.68 2.2 8.49 8.49 0 012 5.69 8.63 8.63 0 01-1.78 5.38 7.6 7.6 0 01-5.89 2.49zm0-3.67c2.42 0 2.73-3 2.73-4.23s-.31-4.26-2.73-4.26-2.79 3-2.79 4.26.32 4.23 2.82 4.23zM95.49 24.37a7.82 7.82 0 01-5.73-2.25 8.36 8.36 0 01-2-5.62 8.32 8.32 0 012.08-5.71 8.4 8.4 0 0111.31 0 8.43 8.43 0 012 5.69 8.6 8.6 0 01-1.77 5.38 7.6 7.6 0 01-5.89 2.51zm0-3.67c2.42 0 2.73-3 2.73-4.23s-.31-4.26-2.73-4.26-2.8 3-2.8 4.26.31 4.23 2.83 4.23zM77.66 30c-5.74 0-7-3.25-7.23-4.52l4.6-.26c.41.91 1.17 1.41 2.76 1.41a2.45 2.45 0 002.82-2.53v-2.68a7 7 0 01-1.7 1.75 6.12 6.12 0 01-5.85-.08c-2.41-1.37-3-4.25-3-6.66 0-.89.12-3.67 1.45-5.42a5.67 5.67 0 014.64-2.4c1.2 0 3 .25 4.46 2.82V8.81h4.85v15.33a5.2 5.2 0 01-2.12 4.32A9.92 9.92 0 0177.66 30zm.15-9.66c2.53 0 2.81-2.69 2.81-3.91s-.31-4-2.81-4-2.81 2.8-2.81 4 .27 3.91 2.81 3.91zM55.56 3.72h9.81v2.41h-9.81z"
-                            fill="var(--color-contrast-higher)" />
-                        <circle cx="15" cy="15" r="15" fill="var(--color-primary)" />
-                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" viewBox="0 0 371.6 371.6" style="enable-background:new 0 0 371.597 371.597" xml:space="preserve"><path style="fill:#5c6670" d="M249.17 65.9V14.4c0-7.92-6.48-14.4-14.4-14.4h-112.6c-7.93 0-14.4 6.48-14.4 14.4v51.5h141.4zM249.17 305.7v51.5c0 7.92-6.48 14.4-14.4 14.4h-112.6c-7.93 0-14.4-6.48-14.4-14.4v-51.5h141.4z"/><path style="fill:#333e48" d="M102.26 305.7a35.56 35.56 0 0 1-35.52-35.53V101.43a35.56 35.56 0 0 1 35.52-35.53h152.4a35.57 35.57 0 0 1 35.54 35.53v168.74a35.56 35.56 0 0 1-35.53 35.52h-152.4z"/><path style="fill:#1e252b" d="M102.26 289.17c-10.48 0-19-8.52-19-19V101.43c0-10.48 8.52-19 19-19h152.4c10.49 0 19.01 8.52 19.01 19v168.74c0 10.48-8.52 19-19 19h-152.4z"/><path style="fill:#a4a9ad" d="M290.19 127.47h14.66v46.37h-14.66z"/><circle style="fill:#fff" cx="178.47" cy="185.8" r="30.16"/><circle style="fill:#ff6d68" cx="178.47" cy="131.87" r="14.2"/><circle style="fill:#5e96dc" cx="140.33" cy="147.66" r="14.2"/><circle style="fill:#ffc843" cx="124.53" cy="185.8" r="14.2"/><circle style="fill:#0071ce" cx="140.33" cy="223.93" r="14.2"/><circle style="fill:#bf534f" cx="178.47" cy="239.73" r="14.2"/><circle style="fill:#ff6d68" cx="216.6" cy="223.93" r="14.2"/><circle style="fill:#0071ce" cx="232.4" cy="185.8" r="14.2"/><path style="fill:#e0b03b" d="M226.64 157.7a14.2 14.2 0 1 0-20.08-20.07 14.2 14.2 0 0 0 20.08 20.07z"/></svg>
                 </a>
+                <div>
+                    <span class="color-accent font-bold">Top</span><span class="color-primary font-bold">Fitnes</span><span class="color-accent font-bold">Braslet</span>
+                </div>
             </div>
 
             <button class="header-v2__nav-control reset anim-menu-btn js-anim-menu-btn js-tab-focus"
