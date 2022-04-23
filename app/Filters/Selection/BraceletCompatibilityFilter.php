e9b75a6f233b34890cd9892f9740d14b7770700e
Anton Kunaev <kunaevanton25@gmail.com>
2022-04-23 23:32:29 +0300

    Исправить фильтрацию в подборе по совместимости

--- app/Filters/Selection/BraceletCompatibilityFilter.php
+++ app/Filters/Selection/BraceletCompatibilityFilter.php
@@ -9,7 +9,7 @@ class BraceletCompatibilityFilter extends AbstractEloquentFilter
 {
     protected $compatibility;
 
-    public function __constructor($compatibility)
+    public function __construct($compatibility)
     {
         $this->compatibility = $compatibility;
     }
@@ -17,19 +17,18 @@ class BraceletCompatibilityFilter extends AbstractEloquentFilter
     public function apply(Builder $query): Builder
     {
         return $query
-
-        ->when($this->compatibility == 'Android', function ($builder) {
-            $builder->whereJsonContains('compatibility', ['Android 4.3'])
-            ->orWherejsonContains('compatibility', 'Android 4.4')
-            ->orWherejsonContains('compatibility', 'Android 5.0')
-            ->orWherejsonContains('compatibility', 'Android 6.0');
+        ->when($this->compatibility == 'Android', function ($query) {
+            $query->orWhereJsonContains('compatibility', ['Android 4.3'])
+            ->orWherejsonContains('compatibility', ['Android 4.4'])
+            ->orWherejsonContains('compatibility', ['Android 5.0'])
+            ->orWherejsonContains('compatibility', ['Android 6.0']);
         })
-        ->when($this->compatibility == 'iOS', function ($builder) {
-            $builder->whereJsonContains('compatibility', ['iOS 7.0'])
-            ->orWherejsonContains('compatibility', 'iOS 8.0')
-            ->orWherejsonContains('compatibility', 'iOS 8.4')
-            ->orWherejsonContains('compatibility', 'iOS 9.0')
-            ->orWherejsonContains('compatibility', 'iOS 10.0');
+        ->when($this->compatibility == 'iOS', function ($query) {
+            $query->orWhereJsonContains('compatibility', ['iOS 7.0'])
+            ->orWherejsonContains('compatibility', ['iOS 8.0'])
+            ->orWherejsonContains('compatibility', ['iOS 8.4'])
+            ->orWherejsonContains('compatibility', ['iOS 9.0'])
+            ->orWherejsonContains('compatibility', ['iOS 10.0']);
         });
     }
 }
